6b1e0edaf18055014100e5f0dbc4214278020ec5
JuhLima85 <juju_amil@hotmail.com>
2022-07-29 21:19:20 -0300

    Projeto Completo

--- src/main/java/com/juhlima/DataConfiguration.java
+++ src/main/java/com/juhlima/DataConfiguration.java
@@ -3,33 +3,45 @@ package com.juhlima;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
-	
+
+	/*
+	 * Configuração local Mysql 
+	 * 
+	 * @Bean public DataSource dataSource() { DriverManagerDataSource dataSource =
+	 * new DriverManagerDataSource();
+	 * dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); dataSource.setUrl(
+	 * "jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC")
+	 * ; dataSource.setUsername("root"); dataSource.setPassword(""); return
+	 * dataSource; }
+	 * 
+	 * @Bean public JpaVendorAdapter jpaVendorAdapter() { HibernateJpaVendorAdapter
+	 * adapter = new HibernateJpaVendorAdapter();
+	 * adapter.setDatabase(Database.MYSQL); adapter.setShowSql(true);
+	 * adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+	 * adapter.setPrepareConnection(true); return adapter; }
+	 */
+
 	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	public BasicDataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+				+ "?sslmode=require";
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
 
-        return basicDataSource;
-    }
+		return basicDataSource;
+	}
 
 }
